bcec976cbfb65e07cf712b782e69eb803b6e8c4f
SahityaSk <sahitya.sk913181@gmail.com>
2025-08-30 20:31:23 +0530

    blog updated

--- ReactsP/dashboard/src/App.jsx
+++ ReactsP/dashboard/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home';
-import Dboard from './pages/Dboard';
+import Blog from './pages/Blog';
+import Layout from './pages/admin/Layout';
+import Dashboard from './pages/admin/Dashboard';
+import AddBlog from './pages/admin/AddBlog';
+import ListBlog from './pages/admin/ListBlog';
+import Comments from './pages/admin/Comments';
 
 
 const App = () => {
@@ -9,7 +14,13 @@ const App = () => {
     <div>
         <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='/dboard' element={<Dboard/>} />
+          <Route path='/blog/:id' element={<Blog/>} />
+          <Route path='/admin' element={<Layout/>}>
+              <Route index element={<Dashboard/>}/>
+              <Route path='addblog' element={<AddBlog/>}/>
+              <Route path='listblog' element={<ListBlog/>}/>
+              <Route path='comments' element={<Comments/>}/>
+          </Route>
         </Routes>
     </div>
   )
--- ReactsP/dashboard/src/components/Navbar.jsx
+++ ReactsP/dashboard/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className='flex justify-between items-centre py-5 mx-8  sm:mx-20 xl:mx-32'>
-        <img src={assets.logo} alt="logo" className='w-22 sm:w-44' />
-        <button>
-            Login
-            <img src={assets.arrow} alt="arrow" className='w-3'/>
-        </button>
+    <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
+      <img onClick={() => navigate('/')} src={assets.logo} alt="logo" className="w-24 sm:w-44 cursor-pointer"/>
+      <button onClick={() => navigate('/admin')} className="flex items-center gap-2 rounded-full text-lg cursor-pointer bg-yellow-400 text-white px-6 py-2 rounded hover:scale-105 transition-all">
+        Login 
+        <img src={assets.arrow} alt="arrow" className="w-3" />
+      </button>
     </div>
   )
 }
