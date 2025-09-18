1fc3ad0f2889df50f9516e6e6259791c7122fa51
SahityaSk <sahitya.sk913181@gmail.com>
2025-09-18 09:10:00 +0530

    navbar completed

--- ReactsP/Movies Now/frontend/src/components/Navbar.jsx
+++ ReactsP/Movies Now/frontend/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { assets } from '../assets/assets';
-import { MenuIcon, SearchIcon, XIcon } from 'lucide-react';
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from 'lucide-react';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const { user } = useUser();
+    const { openSignIn } = useClerk();
+
+    const navigate = useNavigate();
 
   return (
     <div className='fixed top-0 left-0 w-full flex items-center justify-between z-50 p-6 md:px-16 lg:px-36 py-5'>
@@ -21,18 +26,32 @@ const Navbar = () => {
         <XIcon onClick={() => setIsOpen(!isOpen)}
         className='absolute md:hidden top-6 right-6 w-6 h-6 cursor-pointer'/>
 
-        <Link onClick={() => {scroll(0,0), setIsOpen(false)}} to='/'>Home</Link>
-        <Link onClick={() => {scroll(0,0), setIsOpen(false)}} to='/movies'>Movies</Link>
-        <Link onClick={() => {scroll(0,0), setIsOpen(false)}} to='/'>Theaters</Link>
-        <Link onClick={() => {scroll(0,0), setIsOpen(false)}} to='/'>Releases</Link>
-        <Link onClick={() => {scroll(0,0), setIsOpen(false)}} to='/favorite'>Favorites</Link>
+        <Link onClick={() => {scroll(0,0), setIsOpen(false)}} to='/' className='hover:text-red-400 hover:scale-105'>Home</Link>
+        <Link onClick={() => {scroll(0,0), setIsOpen(false)}} to='/movies' className='hover:text-red-400 hover:scale-105'>Movies</Link>
+        <Link onClick={() => {scroll(0,0), setIsOpen(false)}} to='/' className='hover:text-red-400 hover:scale-105'>Theatres</Link>
+        <Link onClick={() => {scroll(0,0), setIsOpen(false)}} to='/' className='hover:text-red-400 hover:scale-105'>Releases</Link>
+        <Link onClick={() => {scroll(0,0), setIsOpen(false)}} to='/favorite' className='hover:text-red-400 hover:scale-105'>Favorites</Link>
       </div>
 
       <div className='flex items-center gap-8'>
-        <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-red-400 hover:bg-primary-dull 
-            transition rounded-full font-medium cursor-pointer'>
-            Login</button>
+        <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer hover:scale-110 duration-300'/>
+        { 
+          !user ? (
+            <button onClick={openSignIn}
+              className='px-4 py-1 sm:px-7 sm:py-2 bg-red-400 hover:scale-105 duration-300 
+              transition rounded-full font-medium cursor-pointer'>
+              Login
+            </button>
+          ) : (
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action label='My Bookings'  labelIcon={<TicketPlus width={15}/>} 
+                onClick={()=> navigate('/my-bookings')} />
+              </UserButton.MenuItems>
+            </UserButton>
+          )
+        }
+        
       </div>
 
         <MenuIcon onClick={() => setIsOpen(!isOpen)}
